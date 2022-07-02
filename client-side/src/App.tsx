ecfee3cb1231e6937fa461090aa7673ef2dacfbd
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-02 06:20:20 +0500

    Inserting images and routers

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Host from './pages/Host';
+import Podcasts from './pages/Podcasts';
 
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/host" element={<Host />} />
+        <Route path="/podcasts" element={<Podcasts />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
