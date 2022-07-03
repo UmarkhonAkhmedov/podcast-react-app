2642667475018e3dbd1806f1b47581a75b06b4c5
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-03 16:09:54 +0500

    Adding Hero component for About Page

--- client-side/src/pages/About.tsx
+++ client-side/src/pages/About.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../components/main/Hero'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <Hero heading='About Finsweet Podcast' text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua." input={false} buttonText="Subscribe Now!" img="/Img/About/hero__main.svg" space={true} />
+    </div>
   )
 }
 
