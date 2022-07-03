84f0a248e241d3184b4eb89671647fd95e8705d3
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-03 16:10:04 +0500

    Adding Hero component for Home Page

--- client-side/src/pages/Home.tsx
+++ client-side/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../components/main/Hero'
 
 function Home() {
   return (
     <div>
-      Home
+      <Hero heading='Become The Hero Of Your Own Story' text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat." input={true} buttonText="Subscribe" img="/Img/Home/hero__main.svg" space={false} />
     </div>
   )
 }
