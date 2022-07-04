1955043c299e815fc00b00f7287ab0852587e44c
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-04 05:46:19 +0500

    Creating Hero Component for Host Page

---
+++ client-side/src/components/Host/HostHero.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from '../basic/Button'
+
+function HostHero() {
+  return (
+    <div className='main__padding site--hostHero'>
+      <div className='container'>
+        <div className='hostHero'>
+          <div className='hostHero__img'>
+            <img src="/Img/Host/hero__main.svg" alt="Podcast Image" />
+          </div>
+          <div className='hostHero__info'>
+            <h6>About The Host</h6>
+            <h2>Hey there, I’m Andrew Jonson and welcome to my Podcast</h2>
+            <p>Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. </p>
+            <Link to="/about">
+              <Button text="Know More" color={true} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HostHero
--- client-side/src/pages/Host.tsx
+++ client-side/src/pages/Host.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import HostHero from '../components/Host/HostHero'
 import Available from '../components/main/Available'
 
 function Host() {
   return (
     <div>
+      <HostHero />
       <Available />
     </div>
   )
