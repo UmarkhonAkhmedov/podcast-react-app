0090359758ab682b5a69ab66128b9d53843b018f
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-05 16:44:17 +0500

    Adding Story Component for Podcast Page

--- client-side/src/pages/Podcasts.tsx
+++ client-side/src/pages/Podcasts.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Story from '../components/Main/Story'
 import PodHero from '../components/Podcast/PodHero'
 
 function Podcasts() {
   return (
     <div>
       <PodHero />
+      <Story heading='Become The Hero Of Your Own Story' text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mus cras lacus tellus morbi viverra suspendisse ornare. Sit volutpat, volutpat ut netus malesuada enim penatibus non aliquet." linkText='Contact us' background='/Img/Podcasts/contact__background.png' />
     </div>
   )
 }
