a324bfd8ed2625af9095f7172f31103dd33d9f21
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-06 09:22:39 +0500

    Adding Words section for About Page

---
+++ client-side/src/components/About/Words.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Words() {
+  return (
+    <div className='container words__background'>
+      <div className='words'>
+        <h2>A few words about our team</h2>
+        <img src="/Img/About/short__main.png" alt="Hands" />
+        <span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem eget condimentum enim libero ultricies amet odio fringilla. Ut nibh morbi augue porta aliquet commodo. Fermentum auctor lacus eget in ut integer viverra sed. Penatibus tortor consequat, habitasse non nisl.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mus cras lacus tellus morbi viverra suspendisse ornare. Sit volutpat, volutpat ut netus malesuada enim penatibus non aliquet.</p>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Words
--- client-side/src/pages/About.tsx
+++ client-side/src/pages/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Results from '../components/About/Results'
+import Words from '../components/About/Words'
 import Hero from '../components/Main/Hero'
 
 function About() {
@@ -7,6 +8,7 @@ function About() {
     <div>
       <Hero heading='About Finsweet Podcast' text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua." input={false} buttonText="Subscribe Now!" img="/Img/About/hero__main.svg" space={true} />
       <Results />
+      <Words />
     </div>
   )
 }
