2cb05c43d3ea28ffda3364e255e0145bb9d0e905
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-10 06:18:08 +0500

    Adding Origins section for Home Page

---
+++ client-side/src/components/Home/Origins.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Origins() {
+  return (
+    <div className='origins main__padding'>
+      <div className='origins__img'>
+        <img className='origins__img--main' src="/Img/Home/origin__main.png" alt="" />
+        <div>
+          <img src="/Img/Home/origin__youtube.svg" alt="" />
+          <span>350+ Videos</span>
+        </div>
+      </div>
+      <div className='origins__info'>
+        <h2>My origins on <span>YouTube</span> in 2008</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mus cras lacus tellus morbi viverra suspendisse ornare. Sit volutpat, volutpat ut netus malesuada enim penatibus non aliquet.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Origins
--- client-side/src/pages/Home.tsx
+++ client-side/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Maker from '../components/Home/Maker'
+import Origins from '../components/Home/Origins'
 import Available from '../components/Main/Available'
 import Hero from '../components/Main/Hero'
 
@@ -9,6 +10,7 @@ function Home() {
       <Hero heading='Become The Hero Of Your Own Story' text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat." input={true} buttonText="Subscribe" img="/Img/Home/hero__main.svg" space={false} />
       <Available />
       <Maker />
+      <Origins />
     </div>
   )
 }
