8269e945e93870fed1bf645a74979367b8c9c598
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-07-10 17:15:56 +0500

    Adding Recent component to Podcast Page

--- client-side/src/pages/Podcasts.tsx
+++ client-side/src/pages/Podcasts.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Recent from '../components/Main/Recent'
 import Story from '../components/Main/Story'
 import PodHero from '../components/Podcast/PodHero'
 
@@ -6,6 +7,9 @@ function Podcasts() {
   return (
     <div>
       <PodHero />
+      <div className='main__padding'>
+        <Recent />
+      </div>
       <Story heading='Become The Hero Of Your Own Story' text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mus cras lacus tellus morbi viverra suspendisse ornare. Sit volutpat, volutpat ut netus malesuada enim penatibus non aliquet." linkText='Contact us' background='/Img/Podcasts/contact__background.png' />
     </div>
   )
